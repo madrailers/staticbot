d3b3a7bd859e610a991d2bb9a615f0f18ec830f1
Brad Grzesiak <brad@bendyworks.com>
2013-12-30 23:40:41 -0600

    Work on capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,64 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require 'rvm/capistrano'
 set :rvm_ruby_string, '2.1.0'
-set :rvm_type, :user
-
-# Bundler tasks
-require 'bundler/capistrano'
 
 set :application, "staticbot"
-set :repository,  "git@github.com:madrailers/staticbot.git"
+set :repo_url,  "git@github.com:madrailers/staticbot.git"
 
 set :scm, :git
 
 # do not use sudo
 set :use_sudo, false
-set(:run_method) { use_sudo ? :sudo : :run }
 
 # This is needed to correctly handle sudo password prompt
-default_run_options[:pty] = true
+#default_run_options[:pty] = true
 
 set :user, "buildbot"
-set :group, user
-set :runner, user
+set :group, fetch(:user)
+set :runner, fetch(:user)
 
 set :ip, '162.243.90.117'
-set :host, "#{user}@#{ip}"
-role :web, host
-role :app, host
+set :host, "#{fetch(:user)}@#{fetch(:ip)}"
+role :web, fetch(:host)
+role :app, fetch(:host)
 
 set :rails_env, :production
 
 # Where will it be located on a server?
-set :deploy_to, "/home/buildbot/apps/#{application}"
-set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
-set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+set :deploy_to, "/home/buildbot/apps/#{fetch(:application)}"
+set :unicorn_conf, "#{fetch(:deploy_to)}/current/config/unicorn.rb"
+set :unicorn_pid, "#{fetch(:deploy_to)}/shared/pids/unicorn.pid"
 
 # Unicorn control tasks
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    on roles(:web) do |host|
+      if test("[ -f #{fetch(:unicorn_pid)} ]")
+        info "Sending USR2 to Unicorn"
+        execute "kill -USR2 `cat #{fetch(:unicorn_pid)}`"
+      else
+        within current_path do
+          execute "bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D"
+        end
+      end
+    end
   end
+
   task :start do
-    run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+    on roles(:web) do |host|
+      info "Starting Unicorn"
+      execute "cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D"
+    end
   end
+
   task :stop do
-    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+    on roles(:web) do |host|
+      if test("[ -f #{fetch(:unicorn_pid)} ]")
+        info "Stopping Unicorn"
+        execute "kill -QUIT `cat #{fetch(:unicorn_pid)}`"
+      else
+        error "Unicorn PID did not exist"
+      end
+    end
   end
 end
